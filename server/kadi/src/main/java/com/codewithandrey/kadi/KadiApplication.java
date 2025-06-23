1ac031d872faf96a85c5c0ebde11fd46548f783f
Andrey Oliveira <dedey.cardoso@gmail.com>
2025-06-22 22:54:12 -0300

    server: spring

---
+++ server/kadi/src/main/java/com/codewithandrey/kadi/KadiApplication.java
@@ -0,0 +1,13 @@
+package com.codewithandrey.kadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KadiApplication.class, args);
+    }
+
+}
---
+++ server/kadi/src/test/java/com/codewithandrey/kadi/KadiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithandrey.kadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
