118236a995eb40c6c46750d65dc5d4cade121919
andreydedey <dedey.cardoso@gmail.com>
2026-01-09 23:44:23 -0300

    feat: cards

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
 
 export const Dashboard = () => {
   return (
@@ -6,6 +7,28 @@ export const Dashboard = () => {
       <div className="flex justify-between">
         <h1 className="text-xl font-medium">Dashboard</h1>
         <Button className="cursor-pointer">Add Wallet</Button>
+      </div>
+      <div className="flex space-x-5">
+        <Card className="gap-1 border-none w-60">
+          <CardHeader className="margin">
+            <CardTitle className="font-light">Cash</CardTitle>
+          </CardHeader>
+          <CardContent className="font-medium text-xl">670 USD</CardContent>
+        </Card>
+        <Card className="gap-1 border-none w-60">
+          <CardHeader className="margin">
+            <CardTitle className="font-light">Revolut</CardTitle>
+          </CardHeader>
+          <CardContent className="font-medium text-xl">897.01 USD</CardContent>
+        </Card>
+        <Card className="gap-1 border-none w-60">
+          <CardHeader className="margin">
+            <CardTitle className="font-light">Bank of America</CardTitle>
+          </CardHeader>
+          <CardContent className="font-medium text-xl">
+            45581.11 USD
+          </CardContent>
+        </Card>
       </div>
     </>
   );
