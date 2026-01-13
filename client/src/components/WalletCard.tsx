dd92180db25025f7fbdbc16071adb62870e3353d
andreydedey <dedey.cardoso@gmail.com>
2026-01-13 01:11:00 -0300

    wip

---
+++ client/src/components/WalletCard.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface WalletCardProps {
+  title: string;
+  amount: number;
+}
+
+export const WalletCard = ({ title, amount }: WalletCardProps) => {
+  return (
+    <Card className="gap-1 border-none w-60">
+      <CardHeader className="margin">
+        <CardTitle className="font-light">{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-medium text-xl">{amount} USD</CardContent>
+    </Card>
+  );
+};
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import { WalletCard } from "@/components/WalletCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const Dashboard = () => {
   return (
@@ -8,27 +11,50 @@ export const Dashboard = () => {
         <h1 className="text-xl font-medium">Dashboard</h1>
         <Button className="cursor-pointer">Add Wallet</Button>
       </div>
-      <div className="flex space-x-5">
-        <Card className="gap-1 border-none w-60">
-          <CardHeader className="margin">
-            <CardTitle className="font-light">Cash</CardTitle>
-          </CardHeader>
-          <CardContent className="font-medium text-xl">670 USD</CardContent>
-        </Card>
-        <Card className="gap-1 border-none w-60">
-          <CardHeader className="margin">
-            <CardTitle className="font-light">Revolut</CardTitle>
-          </CardHeader>
-          <CardContent className="font-medium text-xl">897.01 USD</CardContent>
-        </Card>
-        <Card className="gap-1 border-none w-60">
-          <CardHeader className="margin">
-            <CardTitle className="font-light">Bank of America</CardTitle>
-          </CardHeader>
-          <CardContent className="font-medium text-xl">
-            45581.11 USD
-          </CardContent>
-        </Card>
+      <div className="flex space-x-5 mb-6">
+        <WalletCard title="Cash" amount={670.0} />
+        <WalletCard title="Revolut" amount={897.01} />
+        <WalletCard title="Bank of america" amount={4581.11} />
+      </div>
+      <div className="flex space-x-8">
+        <div className="flex flex-col gap-3">
+          <h3 className="text-sm text-muted-foreground antialiased">
+            Last Transactions
+          </h3>
+          <Card className="w-xs">
+            <CardContent className="flex items-center gap-6">
+              <div className="flex rounded-full h-3/4 aspect-square items-center justify-center bg-zinc-800">
+                <FontAwesomeIcon
+                  className="text-xs"
+                  icon={faArrowRightArrowLeft}
+                />
+              </div>
+              <div className="flex flex-col">
+                <strong className="text-green-400">+38 USD</strong>
+                <CardDescription>Transfer</CardDescription>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h3 className="text-sm text-muted-foreground antialiased">
+            Total Money
+          </h3>
+          <Card className="w-xs">
+            <CardContent className="flex items-center gap-6">
+              <div className="flex rounded-full h-3/4 aspect-square items-center justify-center bg-zinc-800">
+                <FontAwesomeIcon
+                  className="text-xs"
+                  icon={faArrowRightArrowLeft}
+                />
+              </div>
+              <div className="flex flex-col">
+                <strong className="text-green-400">+38 USD</strong>
+                <CardDescription>Transfer</CardDescription>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </>
   );
